c6e8f02d01a1ecd14bfbec3fd34959f647b8d015
Laurine2905 <laurine.rat@outlook.fr>
2023-01-23 19:58:10 +0100

    enregistrer une Expédition (sans test)

--- src/main/java/comptoirs/dao/ClientRepository.java
+++ src/main/java/comptoirs/dao/ClientRepository.java
@@ -16,7 +16,9 @@ public interface ClientRepository extends JpaRepository<Client, String> {
     // Attention : SUM peut renvoyer NULL si on ne trouve pas d'enregistrement
     // On utilise COALESCE pour renvoyer 0 dans ce cas
     // http://www.h2database.com/html/functions.html#coalesce
-    @Query("SELECT COALESCE(SUM(l.quantite), 0) FROM Ligne l WHERE l.commande.client.code = :clientCode")
+    @Query("SELECT COALESCE(SUM(l.quantite), 0) " +
+            "FROM Ligne l " +
+            "WHERE l.commande.client.code = :clientCode")
     int nombreArticlesCommandesPar(String clientCode);
 
 }
